a70ff16a2c678aa614b83b1a89c7fe97a13bf942
DulinaDineth1589 <dulinadineth2021@gmail.com>
2025-03-20 17:37:20 +0530

    express packages installed

--- practise/server.js
+++ practise/server.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, Node.js!');
+app.get('/', (req, res) => {
+    res.send('Welcome to my web app!');
 });
 
-server.listen(3000, () => console.log('Server running on port 3000'));
+app.listen(3000, () => console.log('Server running on port 3000'));
