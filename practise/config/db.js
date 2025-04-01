e58879b19445e3b019795f50b1d389cbaa9b1214
Kelum-Tharindu <155412271+Kelum-Tharindu@users.noreply.github.com>
2025-04-01 22:29:25 +0530

    dataBase connection

--- practise/config/db.js
+++ practise/config/db.js
@@ -1,15 +1,11 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB Connected...");
-    } catch (err) {
-        console.error(err.message);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`❌ Error: ${error.message}`);
         process.exit(1);
     }
 };
